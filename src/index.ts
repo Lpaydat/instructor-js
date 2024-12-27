d95b3bbe847c7b0bf60e5b6abacb5d2ab7db9b6b
lpaydat <lpaydat@gmail.com>
2024-12-28 03:24:00 +0700

    explicit export some types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Instructor, { InstructorClient } from "./instructor";
 
 export { type InstructorClient }
 export * from "./types"
+export type { Mode, ResponseModel, CompletionMeta } from "./types"
 export * from "./constants/providers"
 
 export default Instructor
